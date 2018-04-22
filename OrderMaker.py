6e1bfe06818d00ab096878187fc407115602658d
ShahrazadJPN <muranjpn@gmail.com>
2018-04-22 13:03:42 +0900

    developing

--- OrderMaker.py
+++ OrderMaker.py
@@ -17,17 +17,31 @@ class OrderMaker(Information):
         data = self.order_base_maker(position_side, position_price)
         uniq_id = time.time()
 
-        self.bitmex.create_limit_order(self.product, data['execution_side'], position_size, data['profit_line'], {
-            'contingencyType': 'OneCancelsTheOther',
-            'clOrdLinkID': uniq_id,
-        })
-        self.bitmex.create_order(self.product, 'StopLimit', data['execution_side'], position_size, data['loss_line'], {
-            'contingencyType': 'OneCancelsTheOther',
-            'stopPx': data['loss_line'] - 5,
-            'orderQty': position_size,
-            'price': data['loss_line'],
-            'clOrdLinkID': uniq_id,
-        })
+        ord1 = self.bitmex.create_limit_order(self.product,
+                                              data['execution_side'],
+                                              position_size,
+                                              data['profit_line'],
+                                              {
+                                               'contingencyType': 'OneCancelsTheOther',
+                                               'clOrdLinkID': uniq_id,
+                                              }
+                                              )
+        ord2 = self.bitmex.create_order(self.product,
+                                        'StopLimit',
+                                        data['execution_side'],
+                                        position_size,
+                                        data['loss_line'],
+                                        {
+                                         'contingencyType': 'OneCancelsTheOther',
+                                         'stopPx': data['trigger'],
+                                         'orderQty': position_size,
+                                         'price': data['loss_line'],
+                                         'clOrdLinkID': uniq_id,
+                                        }
+                                        )
+
+        print('profit order:', ord1)
+        print('loss exec order', ord2)
 
         time.sleep(2)
 
@@ -70,23 +84,38 @@ class OrderMaker(Information):
         opposite_side = 'sell' if first_side == 'buy' else 'buy'
         uniq_id = int(time.time())
 
-        self.bitmex.create_limit_order(self.product, first_side, size, order_price, {           # first order
+        ord1 = self.bitmex.create_limit_order(self.product, first_side, size, order_price, {           # first order
             'contingencyType': 'OneTriggersTheOther',
             'clOrdLinkID': uniq_id,
         })
-        self.bitmex.create_limit_order(self.product, opposite_side, size, data['profit_line'], {    # profit order
-            'contingencyType': 'OneCancelsTheOther',
-            'clOrdLinkID': uniq_id,
-        })
-        self.bitmex.create_order(self.product, 'StopLimit', opposite_side, size, data['loss_line'], {   # loss order
-            'contingencyType': 'OneCancelsTheOther',
-            'stopPx': data['loss_line'] - 5,
-            'orderQty': size,
-            'price': data['loss_line'],
-            'clOrdLinkID': uniq_id,
-        })
+        ord2 = self.bitmex.create_limit_order(self.product,
+                                              opposite_side,
+                                              size,
+                                              data['profit_line'],
+                                              {                          # profit order
+                                               'contingencyType': 'OneCancelsTheOther',
+                                               'clOrdLinkID': uniq_id,
+                                              }
+                                              )
+        ord3 = self.bitmex.create_order(self.product,
+                                        'StopLimit',
+                                        opposite_side,
+                                        size,
+                                        data['loss_line'],
+                                        {                      # loss order
+                                         'contingencyType': 'OneCancelsTheOther',
+                                         'stopPx': data['trigger'],
+                                         'orderQty': size,
+                                         'price': data['loss_line'],
+                                         'clOrdLinkID': uniq_id,
+                                        }
+                                        )
 
-        print("ordered: " + str(first_side) + str(size) + " at the price of " + str(order_price))
+        print('first order:', ord1)
+        print('oco no.1 order:', ord2)
+        print('oco no.2 order:', ord3)
+
+        print("ordered: " + str(first_side).capitalize() + ' ' + str(size) + " USD at " + str(order_price))
         self.recorder.balance_recorder(balance, order_price, uniq_id)
         time.sleep(3)
 
@@ -94,6 +123,7 @@ class OrderMaker(Information):
 
         profit = None
         loss = None
+        trigger = None
         opposite = None
 
         if order_side == "buy":
@@ -102,6 +132,7 @@ class OrderMaker(Information):
 
             profit = float(order_price + self.profit_price)
             loss = float(order_price - self.lost_price)
+            trigger = profit + 5
 
         elif order_side == "sell":
 
@@ -109,7 +140,8 @@ class OrderMaker(Information):
 
             profit = float(order_price - self.profit_price)
             loss = float(order_price + self.lost_price)
+            trigger = profit - 5
 
-        data = {'execution_side': opposite, 'loss_line': loss, 'profit_line': profit}
+        data = {'execution_side': opposite, 'loss_line': loss, 'profit_line': profit, 'trigger': trigger}
 
         return data
